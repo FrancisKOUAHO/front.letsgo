1a68075cee5360ff7b266774b1af3226b7628749
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-09-01 22:27:43 +0200

    update yarn

--- config/api.ts
+++ config/api.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { loadStripe } from '@stripe/stripe-js';
+import 'dotenv/config'
+
 
 export const api = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}`,
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
 });
 
-export const baseUrl = `${process.env.REACT_APP_API_UPLOAD_URL}`;
+export const baseUrl = `${process.env.NEXT_PUBLIC_API_UPLOAD_URL}`;
 
-export const STRIPE_PUBLIC_KEY = `${process.env.REACT_APP_STRIPE_PUBLIC_KEY}`;
+export const STRIPE_PUBLIC_KEY = `${process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}`;
 
 export const STRIPE_TEST_PROMISE = loadStripe(STRIPE_PUBLIC_KEY);
