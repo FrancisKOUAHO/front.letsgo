232bb0b211009f7e8e23898054f9ebea84f1696a
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-09-05 16:13:31 +0200

    delete .env

--- config/api.ts
+++ config/api.ts
@@ -4,11 +4,11 @@ import 'dotenv/config'
 
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:3333/api/v1/",
+    baseURL: "https://backendletsgo-preproduction.up.railway.app/api/v1/",
     //baseURL: "http://127.0.0.1:3333/api/v1/",
 });
 
-export const baseUrl = "http://127.0.0.1:3333/uploads/";
+export const baseUrl = "https://backendletsgo-preproduction.up.railway.app/uploads/";
 //export const baseUrl = "http://127.0.0.1:3333/uploads/";
 
 export const STRIPE_PUBLIC_KEY = "pk_test_51MdvGWEkovoeS1CWWQk30YnyudXkuXtJ4l1n3CKDmDAn1E5hG66vzrQQR9vqBssaEook290zHYOLAFTydDzm8ODw00UO7ivtC8";
