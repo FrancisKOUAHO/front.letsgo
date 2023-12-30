2d26a6222cd3fe463ed3977b4ba8a437f860034f
Francis KOUAHO <f.kouaho@skipperndt.com>
2023-12-30 20:51:11 +0100

    production

--- config/api.ts
+++ config/api.ts
@@ -11,6 +11,7 @@ export const api = axios.create({
 export const baseUrl = "https://api.letsgoeurope.fr/uploads/";
 //export const baseUrl = "http://127.0.0.1:3333/uploads/";
 
-export const STRIPE_PUBLIC_KEY = "pk_test_51MdvGWEkovoeS1CWWQk30YnyudXkuXtJ4l1n3CKDmDAn1E5hG66vzrQQR9vqBssaEook290zHYOLAFTydDzm8ODw00UO7ivtC8";
+export const STRIPE_PUBLIC_KEY = "pk_live_51MdvGWEkovoeS1CWUCORErQT2jSzP99IG1psfPDIZq7KwAKefiDztQuUsaa3fYDz5TiKO7v5VtSbfAQTDZ36MIxF00gngPuV0x";
+//export const STRIPE_PUBLIC_KEY = "pk_test_51MdvGWEkovoeS1CWWQk30YnyudXkuXtJ4l1n3CKDmDAn1E5hG66vzrQQR9vqBssaEook290zHYOLAFTydDzm8ODw00UO7ivtC8";
 
 export const STRIPE_TEST_PROMISE = loadStripe(STRIPE_PUBLIC_KEY);
